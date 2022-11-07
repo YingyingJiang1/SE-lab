26f869ba43ebf0111bdea2c826ff6be314dd4cb2
pursue135 <jiangyingying1031@outlook.com>
2022-11-07 20:15:34 +0800

    implement class GenProgramInput, waiting to test"

--- gen-input/genInput.h
+++ gen-input/genInput.h
@@ -1,15 +1,22 @@
 #ifndef INPUT_H
 #define INPUT_H
 #include <string>
-using std::string;
+using namespace std;
 
 
-class GenInput
+class GenProgramInput
 {
+private:
+    //Generate a random integer which is between int 'minInt' and int 'maxInt', append it to string 'target'
+    void genInt(int minInt, int maxInt, string& target);
+    // Generate a random letter, append it to string 'target'
+    void genChar(string& target);
+    // Generate a random string whose length is between length 'minLen' and length 'maxLen', append it to string "target"
+    void genString(int minLen, int maxLen, string& target);
 public:
-    /* Generate a single input for the program based on the input format file and return it as a string, 
-    each input element is separated by a breakline*/
-    string genInput(const string& inputFormatFile);    
+    /* Generate a single input for the program based on the input format file and return it as a string,
+   store the result in 'programInput'. Each input element is separated by a breakline*/
+    bool genInput(const string& inputFormatFile, string& programInput);    
 };
 
 
